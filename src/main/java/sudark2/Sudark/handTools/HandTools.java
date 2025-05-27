7051b7602e19eca82cdb92de4756ea0eb939aa39
SUDARK <2963502563@qq.com>
2025-05-27 15:30:40 +0800

    Sudark

---
+++ src/main/java/sudark2/Sudark/handTools/HandTools.java
@@ -0,0 +1,17 @@
+package sudark2.Sudark.handTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HandTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
